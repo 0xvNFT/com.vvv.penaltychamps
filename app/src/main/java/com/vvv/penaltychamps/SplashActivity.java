c382ac3219730543b54de320c0098610f380aa3b
Jayson <kanekiken101017@gmail.com>
2023-09-14 12:03:35 +0800

    Initial

---
+++ app/src/main/java/com/vvv/penaltychamps/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.vvv.penaltychamps;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIMEOUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+
+            finish();
+        }, SPLASH_TIMEOUT);
+    }
+}
