b65060ab64c639b8ddde24386eab105a66b086a4
Jayson <kanekiken101017@gmail.com>
2023-09-14 14:03:06 +0800

    modification

--- app/src/main/java/com/vvv/penaltychamps/MainActivity.java
+++ app/src/main/java/com/vvv/penaltychamps/MainActivity.java
@@ -1,15 +1,22 @@
 package com.vvv.penaltychamps;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
         setContentView(R.layout.activity_main);
     }
 }
